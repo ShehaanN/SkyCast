427c8d9b6bae40c994981eec35544bc9c463fed5
ShehaanN <shehannadeesha2580@gmail.com>
2025-03-22 00:39:32 +0530

    Add geo API to Search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "5d03571370msha103475cbbed72ap17cc1ajsnd7bac5904cce",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
